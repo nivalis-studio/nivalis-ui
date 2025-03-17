9b6de386d96a3c0e60eb3478fa92b57680c83442
pnodet <paul.nodet@gmail.com>
2025-03-17 10:33:31 +0100

    chore: add storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,15 @@
+import type { Preview } from "@storybook/react";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /date$/i,
+      },
+    },
+  },
+};
+
+// eslint-disable-next-line import/no-default-export
+export default preview;
