f18ea5e23795fdff3771a5e09f5b13d5b0712c3b
Paul Nodet <5941125+pnodet@users.noreply.github.com>
2025-11-11 12:06:50 +0100

    feat: styles

--- registry/niv/index.ts
+++ registry/niv/index.ts
@@ -1,8 +1,9 @@
+import { theme } from './theme/_registry';
 import { ui } from './ui/_registry';
 import type { Registry } from 'shadcn/schema';
 
 export const registry = {
   'name': '@nivalis/ui',
   'homepage': 'https://ui.nivalis.studio',
-  items: [...ui],
+  items: [...ui, ...theme],
 } satisfies Registry;
---
+++ registry/niv/theme/_registry.ts
@@ -0,0 +1,25 @@
+import { colors } from './colors';
+import type { Registry } from 'shadcn/schema';
+
+export const theme: Registry['items'] = [
+  {
+    name: 'theme:medusa',
+    type: 'registry:style',
+    css: {
+      ':root': {
+        '--radius': '0.625rem',
+        '--ease-in-out-quart': 'cubic-bezier(0.76, 0, 0.24, 1)',
+      },
+      '@layer base': {
+        'html, body, #__next': {
+          color: 'var(--color-fg-base)',
+          'border-color': 'var(--color-border-base)',
+          'background-color': 'var(--color-bg-base)',
+        },
+      },
+    },
+    cssVars: {
+      ...colors,
+    },
+  },
+];
